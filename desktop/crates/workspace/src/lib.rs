281e6999c83b41085bd8725341d2c3ca1ae8cdc8
Joris <joris.gallot18@gmail.com>
2026-01-24 17:26:48 +0100

    feat: cmd enter to commit

--- desktop/crates/workspace/src/lib.rs
+++ desktop/crates/workspace/src/lib.rs
@@ -3,5 +3,5 @@ mod git_page;
 mod settings_page;
 mod workspace;
 
-pub use git_page::{OpenRepository, SaveFile};
+pub use git_page::{CommitChanges, OpenRepository, SaveFile};
 pub use workspace::WorkspaceView;
