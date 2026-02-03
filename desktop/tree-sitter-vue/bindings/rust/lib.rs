7a57da7b3a59ebe09ccd21aae0662020c1366128
Joris <joris.gallot18@gmail.com>
2026-02-03 21:57:27 +0100

    chore: vue tree sitter remove test

--- desktop/tree-sitter-vue/bindings/rust/lib.rs
+++ desktop/tree-sitter-vue/bindings/rust/lib.rs
@@ -21,7 +21,7 @@
 use tree_sitter_language::LanguageFn;
 
 extern "C" {
-    fn tree_sitter_vue() -> *const ();
+  fn tree_sitter_vue() -> *const ();
 }
 
 /// The tree-sitter [`LanguageFn`] for this grammar.
@@ -47,14 +47,3 @@ pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
 #[cfg(with_tags_query)]
 /// The symbol tagging query for this grammar.
 pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn test_can_load_grammar() {
-        let mut parser = tree_sitter::Parser::new();
-        parser
-            .set_language(&super::LANGUAGE.into())
-            .expect("Error loading Vue parser");
-    }
-}
