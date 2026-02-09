5c164423da95af0e564a92793399355295d4798a
Joris <joris.gallot18@gmail.com>
2026-02-09 22:56:28 +0100

    chore: graceful shutdown node backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,26 @@
+import process from 'node:process'
 import { serve } from '@hono/node-server'
 import { app } from './app.js'
 import { logger } from './lib/logger.js'
 import './lib/env.js'
 
-serve({
+const server = serve({
   fetch: app.fetch,
   port: 3000,
 }, (info) => {
   logger.info(`Server is running on ${info.port}`)
+})
+
+process.on('SIGINT', () => {
+  server.close()
+  process.exit(0)
+})
+process.on('SIGTERM', () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    process.exit(0)
+  })
 })
