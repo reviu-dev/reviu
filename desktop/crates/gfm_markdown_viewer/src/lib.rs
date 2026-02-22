f73ce065349230b72c8b79874b8e245593ba6a5d
Joris Gallot <joris.gallot18@gmail.com>
2026-02-22 18:42:37 +0100

    feat: code bloc editeur

--- desktop/crates/gfm_markdown_viewer/src/lib.rs
+++ desktop/crates/gfm_markdown_viewer/src/lib.rs
@@ -4,5 +4,6 @@ pub use gfm_markdown_viewer::{
   GithubBlobLineReference, GithubCodeReferencePreview, LinkAction, MarkdownRenderOptions,
   MarkdownRenderState, ParsedMarkdown, estimate_markdown_height_px,
   estimate_parsed_markdown_height_px, extract_github_blob_line_references, parse_markdown,
-  parse_github_blob_line_reference, render_markdown, render_parsed_markdown,
+  parse_github_blob_line_reference, render_github_code_reference_preview_card, render_markdown,
+  render_parsed_markdown,
 };
