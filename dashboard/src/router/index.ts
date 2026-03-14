483f17a1652c0ab426494bd8c402d8327b4d6c41
Joris Gallot <joris.gallot18@gmail.com>
2026-03-14 14:43:28 +0100

    feat:dashboard admin guard

--- dashboard/src/router/index.ts
+++ dashboard/src/router/index.ts
@@ -1,7 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useAuthStore } from '@/stores/auth'
 import { routes } from './routes'
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
+})
+
+router.beforeEach((to, from, next) => {
+  const { me } = useAuthStore()
+
+  if (to.name !== 'Signin' && me.value?.role !== 'admin') {
+    next({ name: 'Signin' })
+  }
+  else if (to.name === 'Signin' && me.value) {
+    next({ name: 'Home' })
+  }
+  else {
+    next()
+  }
 })
