a464eb8002b46aa01dbad29462c9d066a8caf596
Joris Gallot <joris.gallot18@gmail.com>
2026-03-15 21:07:58 +0100

    fix: polar server for prod

--- backend/src/lib/polar.ts
+++ backend/src/lib/polar.ts
@@ -2,6 +2,6 @@ import { Polar } from '@polar-sh/sdk'
 import { env } from './env.js'
 
 export const polarClient = new Polar({
-  server: 'sandbox',
+  server: env.NODE_ENV === 'production' ? 'production' : 'sandbox',
   accessToken: env.POLAR_ACCESS_TOKEN,
 })
