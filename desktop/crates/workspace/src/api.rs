653317e1d335fe1f8e0aac5bea9002a100fcab2b
Joris Gallot <joris.gallot18@gmail.com>
2026-03-23 23:27:28 +0100

    feat: add pro role for free pro features

--- desktop/crates/workspace/src/api.rs
+++ desktop/crates/workspace/src/api.rs
@@ -99,6 +99,7 @@ struct GithubNotificationsResponse {
 #[serde(rename_all = "lowercase")]
 pub enum UserRole {
   User,
+  Pro,
   Admin,
 }
 
@@ -169,7 +170,7 @@ pub struct User {
 
 impl User {
   pub fn has_pro_access(&self) -> bool {
-    matches!(self.role, UserRole::Admin) || self.subscription.active_subscription.is_some()
+    matches!(self.role, UserRole::Admin | UserRole::Pro) || self.subscription.active_subscription.is_some()
   }
 }
 
