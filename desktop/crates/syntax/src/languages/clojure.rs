ed66bdf9c13c490b3476f980b4e664ec4f3073d4
Joris Gallot <joris.gallot18@gmail.com>
2026-08-21 17:46:45 +0200

    chore(deps): update tree-sitter to 0.26 and swap in the maintained clojure grammar

--- desktop/crates/syntax/src/languages/clojure.rs
+++ desktop/crates/syntax/src/languages/clojure.rs
@@ -4,7 +4,7 @@ use once_cell::sync::Lazy;
 pub static CLOJURE_CONFIG: Lazy<LanguageConfig> = Lazy::new(|| {
   build_language_config(
     "clojure",
-    tree_sitter_clojure::LANGUAGE.into(),
+    tree_sitter_clojure_orchard::LANGUAGE.into(),
     &[include_str!(
       "../tree-sitter-queries/clojure-highlights.scm"
     )],
